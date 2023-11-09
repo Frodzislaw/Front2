4e6dfabedfc9741ab13e2abf3667ecc1d06b8750
Frodzislaw <frodzislaw2001@onet.eu>
2023-11-09 09:40:58 +0100

    second commit

---
+++ zad2/events.js
@@ -0,0 +1,9 @@
+let number = 0;
+const zwieksz = document.getElementById('zwiekszanie')
+const napis = document.getElementById('cyferka')
+
+cyfra.addEventListener('click', e => {
+    number += 1;
+    napis.innerText = number;
+})
+
